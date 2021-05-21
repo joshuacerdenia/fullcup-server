4d25b9ad43f9f7a8f7fb1e767e645f9a9d893ec8
Joshua Cerdenia <joshcerdenia@gmail.com>
2021-05-21 18:18:28 +0800

    cleanup

--- controllers/calendar.js
+++ controllers/calendar.js
@@ -4,107 +4,107 @@ const {getAuthorization, getAccessToken} = require('../helpers/auth');
 const moment = require('moment');
 
 const googleCalendar = () => {
-	// Load client secrets from local file
-	const cred = JSON.parse(fs.readFileSync('credentials.json'));
-	// Authorize a client with credentials
-	const auth = getAuthorization(cred);
-	// Call Google Calendar API
-	return google.calendar({version: 'v3', auth});
+  // Load client secrets from local file
+  const cred = JSON.parse(fs.readFileSync('credentials.json'));
+  // Authorize a client with credentials
+  const auth = getAuthorization(cred);
+  // Call Google Calendar API
+  return google.calendar({version: 'v3', auth});
 }
 
 function getCalendarList() {
-	const gc = googleCalendar();
-	return gc.calendarList
-	.list()
-	.then((res, err) => {
-		if (err) return false;
-		const calendars = res.data.items.map((calendar) => {
-			return {
-				id: calendar.id,
-				summary: calendar.summary,
-				description: calendar.description
-			}
-		})
-		
-		return { data: calendars }
-	});
+  const gc = googleCalendar();
+  return gc.calendarList
+  .list()
+  .then((res, err) => {
+    if (err) return false;
+    const calendars = res.data.items.map((calendar) => {
+      return {
+        id: calendar.id,
+        summary: calendar.summary,
+        description: calendar.description
+      }
+    })
+    
+    return { data: calendars }
+  });
 }
 
 function getBusyTimes(calendars = [], days) {
-	const gc = googleCalendar();
-	return gc.freebusy
-	.query({ requestBody: {
-		timeMin: moment().format(),
-		timeMax: moment().add(days, 'days').format(),
-		items: calendars.map((calendar) => {
-			return { id: calendar.id }
-		})}
-	})
-	.then((res, err) => {
-		if (err) return false;
-		const busyTimes = [];
-		for (const id in res.data.calendars) {
-			res.data.calendars[id].busy.forEach((busyTime) => {
-				if (!busyTimes.includes(busyTime)) busyTimes.push({
-					start: moment(busyTime.start).format(),
-					end: moment(busyTime.end).format()
-				});
-			})
-		};
-		
-		return { 
-			data: busyTimes.sort((a, b) => { 
-				return new Date(a.start) - new Date(b.start)
-			}) 
-		}
-	});
+  const gc = googleCalendar();
+  return gc.freebusy
+  .query({ requestBody: {
+    timeMin: moment().format(),
+    timeMax: moment().add(days, 'days').format(),
+    items: calendars.map((calendar) => {
+      return { id: calendar.id }
+    })}
+  })
+  .then((res, err) => {
+    if (err) return false;
+    const busyTimes = [];
+    for (const id in res.data.calendars) {
+      res.data.calendars[id].busy.forEach((busyTime) => {
+        if (!busyTimes.includes(busyTime)) busyTimes.push({
+          start: moment(busyTime.start).format(),
+          end: moment(busyTime.end).format()
+        });
+      })
+    };
+    
+    return { 
+      data: busyTimes.sort((a, b) => { 
+        return new Date(a.start) - new Date(b.start)
+      }) 
+    }
+  });
 }
 
 function getAllBusyTimes(days) {
-	return this.getCalendarList()
-	.then((calendars) => {
-		return (calendars.data) ? getBusyTimes(calendars.data, days) : false
-	});
+  return this.getCalendarList()
+  .then((calendars) => {
+    return (calendars.data) ? getBusyTimes(calendars.data, days) : false
+  });
 }
 
 function getEvents(calendarId = 'primary') {
-	const gc = googleCalendar();
-	return gc.events
-	.list({
-		calendarId: calendarId,
-		timeMin: (new Date()).toISOString(),
-		maxResults: 10,
-		singleEvents: true,
-		orderBy: 'startTime',
-	})
-	.then((res, err) => {
-		if (err) return false;
-		const events = res.data.items.map((event) => {
-			return {
-				summary: event.summary,
-				start: event.start.dateTime,
-				end: event.end.dateTime,
-				creator: event.creator
-			}  
-		});
-		
-		return { data: events }
-	});
+  const gc = googleCalendar();
+  return gc.events
+  .list({
+    calendarId: calendarId,
+    timeMin: (new Date()).toISOString(),
+    maxResults: 10,
+    singleEvents: true,
+    orderBy: 'startTime',
+  })
+  .then((res, err) => {
+    if (err) return false;
+    const events = res.data.items.map((event) => {
+      return {
+        summary: event.summary,
+        start: event.start.dateTime,
+        end: event.end.dateTime,
+        creator: event.creator
+      }  
+    });
+    
+    return { data: events }
+  });
 }
 
 function addCalendar() {
-	const gc = googleCalendar();
-	return gc.calendars
-	.insert({ requestBody: { summary: "Full Cup" }})
-	.then((res, err) => {
-		return (err) ? false : true;
-	});
+  const gc = googleCalendar();
+  return gc.calendars
+  .insert({ requestBody: { summary: "Full Cup" }})
+  .then((res, err) => {
+    return (err) ? false : true;
+  });
 }
 
 module.exports = {
-	getCalendarList,
-	getBusyTimes,
-	getAllBusyTimes,
-	getEvents,
-	addCalendar
+  getCalendarList,
+  getBusyTimes,
+  getAllBusyTimes,
+  getEvents,
+  addCalendar
 }
--- routes/calendar.js
+++ routes/calendar.js
@@ -3,35 +3,35 @@ const router = express.Router();
 const CalendarController = require('../controllers/calendar');
 
 router.get('/get-calendar-list', (req, res) => {
-	CalendarController
-	.getCalendarList()
-	.then((result) => res.send(result));
+  CalendarController
+  .getCalendarList()
+  .then((result) => res.send(result));
 });
 
 router.post('/get-busy-times', (req, res) => {
-	const {calendars, days} = req.body;
-	CalendarController
-		.getBusyTimes(calendars, days) // for testing only
-		.then((result) => res.send(result));
-	});
+  const {calendars, days} = req.body;
+  CalendarController
+  .getBusyTimes(calendars, days)
+  .then((result) => res.send(result));
+});
 
 router.post('/get-all-busy-times', (req, res) => {
-	const days = req.body.days;
-	CalendarController
-	.getAllBusyTimes(days)
-	.then((result) => res.send(result));
+  const days = req.body.days;
+  CalendarController
+  .getAllBusyTimes(days)
+  .then((result) => res.send(result));
 })
 
 router.get('/get-events', (req, res) => {
-	CalendarController
-	.getEvents(undefined)
-	.then((result) => res.send(result));
+  CalendarController
+  .getEvents(undefined)
+  .then((result) => res.send(result));
 });
 
 router.post('/add-calendar', (req, res) => {
-	CalendarController
-	.addCalendar()
-	.then((result) => res.send(result));
+  CalendarController
+  .addCalendar()
+  .then((result) => res.send(result));
 })
 
 module.exports = router;
